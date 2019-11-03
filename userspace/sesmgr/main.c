2116d711e464a6fd396ed022b5341d1cd2b2e370
CHOSTeam <italolmm2018@outlook.com>
2019-11-03 20:19:01 -0300

    Fixed some bugs in the Read/Write functions of the IDE and the AHCI driver x86 ELF loading is now working x86-64 ELF loading is working, but when trying to jump to the loaded code, it gives a general protection fault

--- userspace/sesmgr/main.c
+++ userspace/sesmgr/main.c
@@ -1,19 +1,19 @@
 // File author is Ítalo Lima Marconato Matias
 //
 // Created on October 31 of 2019, at 18:03 BRT
-// Last edited on October 31 of 2019, at 18:08 BRT
+// Last edited on November 03 of 2019, at 16:06 BRT
 
 #include <chicago/file.h>
 
 Void AppEntry(Void) {
-	WChar str[] = L"Hello, World!";							// The string that we are going to write
-	IntPtr con = FsOpenFile(L"/Devices/Console");			// Try to open the console file
+	WChar str[] = L"Hello from /System/Programs/sesmgr.che!";				// The string that we are going to write
+	IntPtr con = FsOpenFile(L"/Devices/Console");							// Try to open the console file
 	
 	if (con == -1) {
-		goto h;												// Failed...
+		goto h;																// Failed...
 	}
 	
-	FsWriteFile(con, sizeof(str), (PUInt8)str);				// Write the string to the screen!
+	FsWriteFile(con, sizeof(str), (PUInt8)str);								// Write the string to the screen!
 	
-h:	while (True) ;											// And, for now, as we don't have anything to do, halt
+h:	while (True) ;															// And, for now, as we don't have anything to do, halt
 }
